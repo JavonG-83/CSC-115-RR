c34119912f64b3b325dcbe0e1dde5212ad6e1936
JavonG-83 <greenj8783@student.faytechcc.edu>
2026-03-18 16:41:44 +0000

    progress

---
+++ M4CW1_Green.java
@@ -0,0 +1,30 @@
+//Javon Green
+//
+
+import java.util.Scanner;
+
+public class M4CW1_Green{public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    //DECLARE VARIABLES
+    int majors, nonMajors;
+    double total, percentMajors, percentNonMajors;
+
+    //OBTAIN INPUTS
+    System.out.print("\nEnter the number of students majoring in CS: ");
+    majors = input.nextInt();
+    System.out.print("Enter the number of non-computer science students: ");
+    nonMajors = input.nextInt();
+
+    //CALCULATE
+    total = majors + nonMajors;
+    percentMajors = (majors / total) * 100;
+    percentNonMajors = (nonMajors / total) * 100;
+
+    //OUTPUT RESULTS
+    System.out.println("Majors: " + percentMajors + "%");
+    System.out.println("Nonmajors: " + percentNonMajors + "%\n");
+
+    input.close();
+}}
